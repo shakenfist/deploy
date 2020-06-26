a4b73c6d016883d5ff63ee41f3fcef2c5c55481c
Michael Still <mikal@stillhq.com>
2020-06-26 17:21:54 +1000

    Drop support for nutanix; move ansible CI aside.

--- ansible/deployandtest.sh
+++ ansible/deployandtest.sh
@@ -9,32 +9,4 @@ do
   ANSIBLE_VARS="$ANSIBLE_VARS $var"
 done
 
-
-# Nutanix is a special case
-if [ "$CLOUD" == "nutanix" ]
-then
-  cd terraform/nutanix/phase-2
-  terraform init
-  terraform destroy -auto-approve $TERRAFORM_VARS -state=../terraform.tfstate
-  cd ..
-  find . -type f -name "*tfstate*" -exec rm {} \;
-  cd ../..
-else
-  cd terraform/$CLOUD
-  terraform init
-  terraform destroy -auto-approve $TERRAFORM_VARS
-  cd $cwd
-fi
-
 ansible-playbook -i hosts --extra-vars "$ANSIBLE_VARS" deploy.yml
-
-if [ "%$SKIP_SF_TESTS%" == "%%" ]
-then
-  ansible-playbook -i hosts --extra-vars "$ANSIBLE_VARS" pretest.yml
-  for playbook in `ls tests/test_*.yml | grep -v test_final.yml | shuf`
-  do
-    ansible-playbook -i hosts --extra-vars "$ANSIBLE_VARS" $playbook
-  done
-fi
-
-ansible-playbook -i hosts --extra-vars "$ANSIBLE_VARS" tests/test_final.yml
