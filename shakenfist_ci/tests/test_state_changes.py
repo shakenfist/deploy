12095d0fea1ca4d60f02fad3fb21cbfb497703f6
Michael Still <mikal@stillhq.com>
2020-07-05 08:32:31 +1000

    Test state changes in python CI.

---
+++ shakenfist_ci/tests/test_state_changes.py
@@ -0,0 +1,106 @@
+import base64
+import time
+
+from oslo_concurrency import processutils
+from shakenfist.client import apiclient
+
+from shakenfist_ci import base
+
+
+class TestStateChanges(base.BaseTestCase):
+    def setUp(self):
+        super(TestStateChanges, self).setUp()
+
+        self.namespace = 'ci-multinic-%s' % self._uniquifier()
+        self.namespace_key = self._uniquifier()
+        self.test_client = self._make_namespace(
+            self.namespace, self.namespace_key)
+        self.net_one = self.test_client.allocate_network(
+            '192.168.242.0/24', True, True, '%s-net-one' % self.namespace)
+        self.net_two = self.test_client.allocate_network(
+            '192.168.243.0/24', True, True, '%s-net-two' % self.namespace)
+
+    def tearDown(self):
+        super(TestStateChanges, self).tearDown()
+        for inst in self.test_client.get_instances():
+            self.test_client.delete_instance(inst['uuid'])
+        for net in self.test_client.get_networks():
+            self.test_client.delete_network(net['uuid'])
+        self._remove_namespace(self.namespace)
+
+    def test_simple(self):
+        inst = self.test_client.create_instance(
+            'cirros', 1, 1,
+            [
+                {
+                    'network_uuid': self.net_one['uuid']
+                },
+                {
+                    'network_uuid': self.net_two['uuid']
+                }
+            ],
+            [
+                {
+                    'size': 8,
+                    'base': 'cirros',
+                    'type': 'disk'
+                }
+            ], None, None)
+        ip = self.test_client.get_instance_interfaces(inst['uuid'])[0]['ipv4']
+
+        self.assertIsNotNone(inst['uuid'])
+        self._await_login_prompt(inst['uuid'])
+
+        # Soft reboot
+        self.test_client.reboot_instance(inst['uuid'])
+        self._await_login_prompt(inst['uuid'])
+        out, _ = processutils.execute(
+            'ip netns exec %s ping -c 1 %s | grep -c " 0%% packet loss"'
+            % (self.net['uuid'], ip),
+            shell=True)
+        self.assertEqual(out.rstrip(), '1')
+
+        # Hard reboot
+        self.test_client.reboot_instance(inst['uuid'], hard=True)
+        self._await_login_prompt(inst['uuid'])
+        out, _ = processutils.execute(
+            'ip netns exec %s ping -c 1 %s | grep -c " 0%% packet loss"'
+            % (self.net['uuid'], ip),
+            shell=True)
+        self.assertEqual(out.rstrip(), '1')
+
+        # Power off
+        self.test_client.power_off_instance(inst['uuid'])
+        time.sleep(5)
+        out, _ = processutils.execute(
+            'ip netns exec %s ping -c 1 %s | grep -c " 0%% packet loss"'
+            % (self.net['uuid'], ip),
+            shell=True)
+        self.assertEqual(out.rstrip(), '0')
+
+        # Power on
+        self.test_client.power_on_instance(inst['uuid'])
+        self._await_login_prompt(inst['uuid'])
+        out, _ = processutils.execute(
+            'ip netns exec %s ping -c 1 %s | grep -c " 0%% packet loss"'
+            % (self.net['uuid'], ip),
+            shell=True)
+        self.assertEqual(out.rstrip(), '1')
+
+        # Pause
+        self.test_client.pause_instance(inst['uuid'])
+        time.sleep(5)
+        out, _ = processutils.execute(
+            'ip netns exec %s ping -c 1 %s | grep -c " 0%% packet loss"'
+            % (self.net['uuid'], ip),
+            shell=True)
+        self.assertEqual(out.rstrip(), '0')
+
+        # Unpause
+        self.test_client.unpause_instance(inst['uuid'])
+        self._await_login_prompt(inst['uuid'])
+        out, _ = processutils.execute(
+            'ip netns exec %s ping -c 1 %s | grep -c " 0%% packet loss"'
+            % (self.net['uuid'], ip),
+            shell=True)
+        self.assertEqual(out.rstrip(), '1')
